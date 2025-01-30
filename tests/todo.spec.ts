1cd3f49cb2fdb9e7b1e5bc30b64fd997ed0030e5
Shita Zeny <shita0639@gmail.com>
2025-01-30 21:44:07 +0700

    test: Add another test spec to the same file and refine our test #1

--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -1,16 +1,34 @@
 import { test, expect } from '@playwright/test';
 
-test('active and completed filters', async ({ page }) => {
-  await page.goto('https://demo.playwright.dev/todomvc/#/');
-  await page.getByRole('textbox', { name: 'What needs to be done?' }).click();
-  await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('water the plants');
-  await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
-  await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('feed the dog');
-  await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
-  await page.getByRole('listitem').filter({ hasText: 'water the plants' }).getByLabel('Toggle Todo').check();
-  await page.getByRole('link', { name: 'Active' }).click();
-  await page.getByRole('link', { name: 'Completed' }).click();
-  await page.getByText('All Active Completed').click();
-  await page.getByRole('link', { name: 'All' }).click();
-  await page.getByRole('listitem').filter({ hasText: 'water the plants' }).getByLabel('Toggle Todo').uncheck();
+test.describe('New Todo', () => {
+  test.beforeEach(async ({ page}) => {
+    await page.goto('');
+  });
+
+  test('active and completed filters', async ({ page }) => {
+    const todoInput = page.getByPlaceholder('What needs to be done?');
+
+    await todoInput.click();
+    await todoInput.fill('water the plants');
+    await todoInput.press('Enter');
+    await todoInput.fill('feed the dog');
+    await todoInput.press('Enter');
+    await page.getByRole('listitem').filter({ hasText: 'water the plants' }).getByLabel('Toggle Todo').check();
+    await page.getByRole('link', { name: 'Active' }).click();
+    await page.getByRole('link', { name: 'Completed' }).click();
+    await page.getByText('All Active Completed').click();
+    await page.getByRole('link', { name: 'All' }).click();
+    await page.getByRole('listitem').filter({ hasText: 'water the plants' }).getByLabel('Toggle Todo').uncheck();
+  });
+  
+  test('Exercise: Manually Create a Test Spec', async ({ page }) => {
+    const todoInput = page.getByPlaceholder('What needs to be done?');
+    
+    await todoInput.fill('water the plants');
+    await todoInput.press('Enter');
+
+    await expect(page.locator('.new-todo')).toBeEmpty();
+
+    await expect(todoInput).toBeEmpty();
+  });
 });
